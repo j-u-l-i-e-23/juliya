a0120d9f65cf93f5126ad01feece000bf419ca94
Me <Me@DESKTOP-5MS65T0.mshome.net>
2020-03-25 12:02:41 +0530

    new commit

--- src/LabUnit7/Fibonacci.java
+++ src/LabUnit7/Fibonacci.java
@@ -9,6 +9,33 @@ package LabUnit7;
  *
  * @author Me
  */
+
+import java.util.*;
+
+
 public class Fibonacci {
+   int fib(int n)
+   {
+       if(n<=1)
+           return 1;
+       else
+           return(fib(n-1)+fib(n-2));
+       
     
+           
+   }
+   public static void main(String args[])
+   {
+       int i;
+       Fibonacci f=new Fibonacci();
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter a number graeter than 2");
+       int n=sc.nextInt();
+       for(i=0;i<n;i++)
+       {
+           int fi=f.fib(i);
+           System.out.println("The fibonnaci number of"+i+"is="+fi );
+       }
+       
+   }
 }
